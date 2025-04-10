ba4a8089cbfb9584b96e5f7519634c7df6465d00
Julio Cesar Bezerra <jcbs3095@gmail.com>
2025-04-10 16:19:43 -0300

    Add project example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,50 +3,24 @@ package org.example
 import org.example.entities.InventoryItem
 
 import org.example.service.createExcelFile
-import org.example.service.readExcelFile
+import functions.readExcelFile
 import org.example.service.removeItemByIndex
 import org.example.service.updateCellValue
 import java.io.File
 
 fun main() {
     val filePath = "src/main/kotlin/files/Inventory.xlsx"
-    val apoiemais = "src/main/kotlin/files/projetos-aptos-a-captacao 17.03.25.xlsx"
 
     // Verifica se o arquivo existe para deletar e criar outro exemplo
     val file = File("src/main/kotlin/files/Inventory.xlsx")
     if (file.exists()) file.delete()
 
-   // Populando entidade para teste
-    val items = listOf(
-        InventoryItem(1, "Teclado", 10, 100.0),
-        InventoryItem(2, "Mouse", 20, 50.0),
-        InventoryItem(3, "Monitor", 5, 900.0),
-        InventoryItem(4, "Gabinete", 7, 300.0),
-        InventoryItem(5, "Fonte de Alimentação", 15, 250.0),
-        InventoryItem(6, "Placa-Mãe", 10, 500.0),
-        InventoryItem(7, "Processador", 8, 1200.0),
-        InventoryItem(8, "Memória RAM", 12, 400.0),
-        InventoryItem(9, "HD Externo", 9, 350.0),
-        InventoryItem(10, "SSD", 14, 600.0),
-        InventoryItem(11, "Placa de Vídeo", 6, 1500.0),
-        InventoryItem(12, "Webcam", 11, 200.0),
-        InventoryItem(13, "Impressora", 4, 700.0),
-        InventoryItem(14, "Scanner", 3, 650.0),
-        InventoryItem(15, "Notebook", 5, 3500.0),
-        InventoryItem(16, "Tablet", 7, 1800.0),
-        InventoryItem(17, "Smartphone", 10, 2500.0),
-        InventoryItem(18, "Cadeira de Escritório", 8, 850.0),
-        InventoryItem(19, "Mesa de Escritório", 6, 1200.0),
-        InventoryItem(20, "Luminária", 15, 150.0)
-    )
 
-    // Funções:
-
-    // Cria o Excel
-    createExcelFile(items, filePath)
+    // Cria o Excel Apartir dos dados de uma entidade
+    createExcelFile(InventoryItem.returnInventory(), filePath)
     println()
 
-    //Remover Linha
+    // Remover Linha
     // Teclado será removido
     removeItemByIndex(filePath, 1)
     println("Deletado:  InventoryItem(1, Teclado, 10, 100.0)")
@@ -59,5 +33,4 @@ fun main() {
 
     //Atualizar dados
     updateCellValue(filePath, 0, 19, 2, "20")
-
 }
--- src/main/kotlin/entities/InventoryItem.kt
+++ src/main/kotlin/entities/InventoryItem.kt
@@ -5,4 +5,33 @@ data class InventoryItem(
     val productName: String,
     val quantityInStock: Int,
     val pricePerUnit: Double
-)
+) {
+    // O campanion object foi adicionado pra simular uma base de dados
+    companion object {
+        fun returnInventory(): List<InventoryItem> {
+            return listOf(
+                InventoryItem(1, "Teclado", 10, 100.0),
+                InventoryItem(2, "Mouse", 20, 50.0),
+                InventoryItem(3, "Monitor", 5, 900.0),
+                InventoryItem(4, "Gabinete", 7, 300.0),
+                InventoryItem(5, "Fonte de Alimentação", 15, 250.0),
+                InventoryItem(6, "Placa-Mãe", 10, 500.0),
+                InventoryItem(7, "Processador", 8, 1200.0),
+                InventoryItem(8, "Memória RAM", 12, 400.0),
+                InventoryItem(9, "HD Externo", 9, 350.0),
+                InventoryItem(10, "SSD", 14, 600.0),
+                InventoryItem(11, "Placa de Vídeo", 6, 1500.0),
+                InventoryItem(12, "Webcam", 11, 200.0),
+                InventoryItem(13, "Impressora", 4, 700.0),
+                InventoryItem(14, "Scanner", 3, 650.0),
+                InventoryItem(15, "Notebook", 5, 3500.0),
+                InventoryItem(16, "Tablet", 7, 1800.0),
+                InventoryItem(17, "Smartphone", 10, 2500.0),
+                InventoryItem(18, "Cadeira de Escritório", 8, 850.0),
+                InventoryItem(19, "Mesa de Escritório", 6, 1200.0),
+                InventoryItem(20, "Luminária", 15, 150.0)
+            )
+        }
+    }
+}
+
